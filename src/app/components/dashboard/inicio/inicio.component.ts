e0f868b9d576cbed7e22c31119db309d4172d266
FlameXS01 <perezjailer41@gmail.com>
2024-04-24 22:54:00 -0500

    finished

--- src/app/components/dashboard/inicio/inicio.component.ts
+++ src/app/components/dashboard/inicio/inicio.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { Categoria } from 'src/app/interface/categoria';
 import { Product } from 'src/app/interface/product';
+import { Ventas } from 'src/app/interface/ventas';
 import { CategoriaService } from 'src/app/services/categoria';
 import { NationalProdService } from 'src/app/services/nationalProd';
 import { ProductService } from 'src/app/services/product';
+import { VentasService } from 'src/app/services/ventas';
 
 @Component({
   selector: 'app-inicio',
@@ -12,8 +14,13 @@ import { ProductService } from 'src/app/services/product';
 })
 export class InicioComponent {
   categories!: Categoria[];
+  ventas! : any[];
+  topSeller: any;
+  mostSellProd : any;
 
-  constructor(private categoriaService: CategoriaService, private productService: NationalProdService, private cdr: ChangeDetectorRef) { }
+  constructor(private categoriaService: CategoriaService, private productService: NationalProdService, private cdr: ChangeDetectorRef, private _ventasService : VentasService) { 
+   
+  }
 
   ngOnInit(): void {
     this.categoriaService.consultarCategoria().subscribe(
@@ -41,5 +48,58 @@ export class InicioComponent {
         console.error(error);
       }
     );
+    this._ventasService.consultarVentas().subscribe(
+      (ventas: Ventas[]) => {
+        this.ventas = ventas;
+        this.topSeller = this.getTopSeller();
+        this.mostSellProd = this.getMostSoldProduct();
+      }
+    );
+    
+  }
+
+  getMostSoldProduct(): string {
+    const productCount: { [name: string]: number } = {};
+
+    for (let venta of this.ventas) {
+      if (!productCount[venta.product.name]) {
+        productCount[venta.product.name] = 0;
+      }
+
+      productCount[venta.product.name]++;
+    }
+    if (Object.keys(productCount).length === 0) {
+      return '';
+    }
+    if(Object.keys(productCount).reduce((a, b) => productCount[a] > productCount[b] ? a : b)){
+      return Object.keys(productCount).reduce((a, b) => productCount[a] > productCount[b] ? a : b);  
+    }
+    else{
+      return 'no hay aunn ' ;
+    }
+
+  }
+
+  getTopSeller(): string {
+    const personCount: { [name: string]: number } = {};
+
+  for (let venta of this.ventas) {
+  if (!personCount[venta.person.name]) {
+    personCount[venta.person.name] = 0;
+  }
+  personCount[venta.person.name]++;
+}
+
+    if (Object.keys(personCount).length === 0) {
+      return '';
+    }
+
+    if(Object.keys(personCount).reduce((a, b) => personCount[a] > personCount[b] ? a : b) != null){
+      return Object.keys(personCount).reduce((a, b) => personCount[a] > personCount[b] ? a : b);  
+    }
+    else{
+      return 'no hay aunn ' ;
+    }
+   
   }
 }
